ea8fe6fd1904340ba7293b7d2f2991ef29cb489c
KhushiKumari27X <khushikumarikh27@gmail.com>
2026-04-01 20:36:24 +0530

    Added .gitignore and initial files

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        Lorem vhgjahihaS.KBGHGJHBKNK
+        SDFGHJKL.kjhgfdserdtfghjkmknjbhgdfghjkmlnbhvgf
+        gfghjlkjftrsrdtfyghnjbhvgf
+        oihugytfrdtfyui
+    </div>
+  )
+}
+
+export default About
+ 
